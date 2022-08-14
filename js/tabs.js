8689f852aed16c41fd8f99a4ed66130f7c93dac2
Guilherme Augusto <guilhermelive980@hotmail.com>
2022-08-14 18:28:27 -0300

    tabs start

---
+++ js/tabs.js
@@ -0,0 +1,8 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tab = document.querySelectorAll('[role="tab"]');
+
+
+tabList.addEventListener('keydown', (e) => {
+    console.log(e);
+} )
+
